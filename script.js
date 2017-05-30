8bdb5d761472fc7eb9d85a0b2124dfca6e30d57b
Shefali Emmanuel <shefali.emmanuel@pokitdok.com>
2017-05-30 10:22:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var myApp = angular.module("myModule", []);
 
-myApp.controller("myController", function ($scope){
+myApp.controller("myController", function ($scope, $http){
     var labInfo = {
         department: "Computer Science",
         facultyMember:"Dr. Jim Bowring",
@@ -10,23 +10,10 @@ myApp.controller("myController", function ($scope){
         description: "CIRDLES is an undergraduate computer science software engineering initiative at the The College of Charleston , Charleston, SC. We specialize in the collaborative development of free open source software to support science domains. This material is based upon work supported by the National Science Foundation under Grant Numbers 0930223 and 1443037."
     };
 
-var output= document.getElementById('output');
-
-var ajaxhttp= new XMLHttpRequest();
-var url= "localStorage.json";
-
-ajaxhttp.open("GET", url, true);
-ajaxhttp.setRequestHeader("content-type", "application/json");
-ajaxhttp.onreadystatechange = function (){
-    if(ajaxhttp.readyState == 4 && ajaxhttp.status == 200){
-    var jcontent = ajaxhttp;
-    console.log(jcontent);
-    console.log(ajaxhttp);
-    }
-}
-
-ajaxhttp.send(null)
-console.log(ajaxhttp)
-
-    $scope.labInfo = labInfo;
+    var output= document.getElementById('output');
+    $http.get('localStorage.json').then(function(res){
+        $scope.labInfo = res.data.localStorage;
+    }, function(err, res){
+        alert('didn\'t work');
+    });
 });
